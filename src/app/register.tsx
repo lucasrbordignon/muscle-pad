31da64793a9825c1ecacbdda96ad09be734ff306
Lucas Rodrigues <lucasrodrigues@MacBook-Air-de-Lucas.local>
2024-10-16 12:40:34 -0300

    add-zod-validation

--- src/app/register.tsx
+++ src/app/register.tsx
@@ -2,13 +2,53 @@ import { Input } from '@/src/components/Input';
 import { Feather } from '@expo/vector-icons';
 import Constants from 'expo-constants';
 import { Link } from 'expo-router';
-import React from 'react';
-import { Dimensions, Keyboard, KeyboardAvoidingView, ScrollView, Text, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
+import React, { useState } from 'react';
+import { Alert, Dimensions, Keyboard, KeyboardAvoidingView, ScrollView, Text, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
+import z from 'zod';
 
 const statusBarHeight = Constants.statusBarHeight
- const paddingTop = Dimensions.get('window').height / 16
+const paddingTop = Dimensions.get('window').height / 16
+
+const formSchema = z.object({
+  name: z.string().min(1, { message: "Nome é obrigatório" }),
+  lastName: z.string().min(1, { message: "Sobrenome é obrigatório" }),
+  mail: z.string().email({ message: "Email inválido" }),
+  password: z.string().min(6, { message: "Senha deve ter pelo menos 6 caracteres" }),
+  confirmPassword: z.string().min(6, { message: "Confirmação de senha é obrigatória" }),
+}).refine((data) => data.password === data.confirmPassword, {
+  path: ["confirmacaoSenha"],
+  message: "Senhas não coincidem",
+});
 
 export default function Register() {
+  const [name, setName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+  const [mail, setMail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('')
+  const [errors, setErrors] = useState({});
+
+  const handleSubmit = () => {
+    const form = {
+      name,
+      lastName,
+      mail,
+      password,
+      confirmPassword
+    }
+
+    const result = formSchema.safeParse(form);
+
+    if (!result.success) {
+      const errorMessages = result.error.errors[0].message
+      Alert.alert("Erros no formulário", errorMessages);
+    } else {
+      // Sucesso
+      Alert.alert("Sucesso", "Formulário válido!");
+      console.log(result.data);
+    }
+  };
+
   return (
     <KeyboardAvoidingView className='flex-1'  behavior="padding">
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -26,14 +66,14 @@ export default function Register() {
 
           <Text className='flex-1 text-center text-zinc-600 font-semibold my-2'>Digite as informações abaixo e venha treinar com a gente!</Text>
           
-          <Input label='Nome' placeholder='Digite seu nome...'/>
-          <Input label='Sobrenome' placeholder='Digite seu sobrenome...'/>
-          <Input label='Email' placeholder='Digite seu email...'/>
-          <Input label='Senha' placeholder='Digite sua senha...'/>
-          <Input label='Confirmar' placeholder='Confirme sua senha...'/>
+          <Input label='Nome' placeholder='Digite seu nome...' value={name} onChangeText={setName}/>
+          <Input label='Sobrenome' placeholder='Digite seu sobrenome...' value={lastName} onChangeText={setLastName}/>
+          <Input label='Email' placeholder='Digite seu email...' value={mail} onChangeText={setMail}/>
+          <Input label='Senha' placeholder='Digite sua senha...' value={password} onChangeText={setPassword} secureTextEntry={true}/>
+          <Input label='Confirmar' placeholder='Confirme sua senha...' value={confirmPassword} onChangeText={setConfirmPassword} secureTextEntry={true}/>
 
           <TouchableOpacity 
-            onPress={() => {}}
+            onPress={handleSubmit}
             className='bg-blue-500 p-3 rounded-xl mt-8 w-full'
           > 
             <View className='flex flex-row items-center justify-center gap-2'>        
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,16 +7,20 @@ interface CustomInputProps extends TextInputProps {
   onChangeText?: (text: string) => void;
   placeholder?: string;
   secureTextEntry?: boolean;
+  key?: string;
 }
 
-export const Input: React.FC<CustomInputProps> = ({label, placeholder,secureTextEntry}) => {
+export const Input: React.FC<CustomInputProps> = ({...CustomInputProps}) => {
   return (
     <View className='w-full flex mt-4 gap-2'>
-      { label && <Text className="text-2xl font-bold text-zinc-800">{label}</Text>}
+      { CustomInputProps.label && <Text className="text-2xl font-bold text-zinc-800">{CustomInputProps.label}</Text>}
       <TextInput
-        placeholder={placeholder}        
+        placeholder={CustomInputProps.placeholder}        
         className='border p-2 w-full rounded-xl'
-        secureTextEntry={secureTextEntry}        
+        secureTextEntry={CustomInputProps.secureTextEntry}     
+        value={CustomInputProps.value}   
+        onChangeText={CustomInputProps.onChangeText}
+        key={CustomInputProps.key}
       />        
     </View>
   )
